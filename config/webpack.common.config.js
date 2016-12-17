59994004b18519b61dc9b1de4f352d10853891d3
Idriss <ihippocrate@gmail.com>
2016-12-17 10:51:39 +0100

    add html-webpack-plugin

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -1,7 +1,8 @@
-var webpack = require('webpack');
-var path = require('path');
-var webpackMerge = require('webpack-merge');
+const webpack = require('webpack');
+const path = require('path');
+const webpackMerge = require('webpack-merge');
 const TsConfigPathsPlugin = require('awesome-typescript-loader').TsConfigPathsPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -9,7 +10,6 @@ module.exports = {
     'main': './src/main.browser.ts',
   },
   output: {
-    publicPath: '',
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].bundle.js',
     sourceMapFilename: '[name].map',
@@ -18,7 +18,7 @@ module.exports = {
 
   resolve: {
     extensions: ['.ts', '.js'],
-    modules: [path.resolve(__dirname, '../node_modules')],
+    modules: [path.resolve(__dirname, '../src'), path.resolve(__dirname, '../node_modules')],
     plugins: [
       new TsConfigPathsPlugin(/* { tsconfig, compiler } */)
     ]
@@ -35,18 +35,33 @@ module.exports = {
         ]
       },
       { test: /\.css$/, loaders: ['to-string-loader', 'css-loader'] },
-      { test: /\.html$/, loader: 'raw-loader' }
+      { 
+        test: /\.html$/, 
+        loader: 'raw-loader',
+        exclude: [path.resolve(__dirname, '../src/index.html')]
+      }
     ]
   },
   plugins: [
-    new webpack.ContextReplacementPlugin(
+      new webpack.ContextReplacementPlugin(
       // The (\\|\/) piece accounts for path separators in *nix and Windows
       /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
       path.resolve(__dirname, '../src'),
       {
         // your Angular Async Route paths relative to this root directory
-      }
-    ),
+      }),
+      /*
+       * Plugin: HtmlWebpackPlugin
+       * Description: Simplifies creation of HTML files to serve your webpack bundles.
+       * This is especially useful for webpack bundles that include a hash in the filename
+       * which changes every compilation.
+       *
+       * See: https://github.com/ampedandwired/html-webpack-plugin
+       */
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      chunksSortMode: 'dependency'
+    })
   ],
   node: {
     global: true,
